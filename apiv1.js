5424eac72158d3568ea3c30f85e01c45f0a29383
ljw980105 <ljw9801055@gmail.com>
2020-03-04 23:54:10 +0000

    Added Https serving

--- apiv1.js
+++ apiv1.js
@@ -1,12 +1,17 @@
 
 var app = require('express')();
 var use_http = require('http');
-var http = use_http.Server(app);
+var https = require('https');
+//var http = use_http.Server(app);
 
 const oauth = require("./lib/google_oauth.js");
 
 const fs = require('fs');
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.get("/apiv1/get_login_link", function (req, res) {
     const oAuth2Client = oauth.getOAuth2Client(JSON.parse(fs.readFileSync("creds.json")));
@@ -17,6 +22,6 @@ app.get("/apiv1/get_login_link", function (req, res) {
 })
 
 
-http.listen(4200, function () {
+https.createServer({key: fs.readFileSync('server.key'), cert: fs.readFileSync('server.cert')}, app).listen(4200, function () {
     console.log('Server up on *:4200');
 });
