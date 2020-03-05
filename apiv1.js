3725bbebfc7cbcc69c6858e7e06d93e833a0221c
MadSnailDisease <madsnaildisease123@gmail.com>
2020-03-05 15:31:24 -0500

    Revert "Added Https serving"
    This reverts commit 5424eac72158d3568ea3c30f85e01c45f0a29383.

--- apiv1.js
+++ apiv1.js
@@ -1,17 +1,12 @@
 
 var app = require('express')();
 var use_http = require('http');
-var https = require('https');
-//var http = use_http.Server(app);
+var http = use_http.Server(app);
 
 const oauth = require("./lib/google_oauth.js");
 
 const fs = require('fs');
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 app.get("/apiv1/get_login_link", function (req, res) {
     const oAuth2Client = oauth.getOAuth2Client(JSON.parse(fs.readFileSync("creds.json")));
@@ -22,6 +17,6 @@ app.get("/apiv1/get_login_link", function (req, res) {
 })
 
 
-https.createServer({key: fs.readFileSync('server.key'), cert: fs.readFileSync('server.cert')}, app).listen(4200, function () {
+http.listen(4200, function () {
     console.log('Server up on *:4200');
 });
